959ebc9c352a86c87c3e3c841a90b8238fd080ad
golden-state0fmind <areyes2009@yahoo.com>
2021-04-26 23:12:25 -0700

    uses new components

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ColorBox from './components/ColorBox';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1}} >
+      <View style={styles.container}>
+        <Text style={styles.text}>Color Legend</Text>
+        <StatusBar style="auto" />
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Magenta" hexCode="#d33682" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Orange" hexCode="#cb4b16" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingTop:40,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom:10,
   },
 });
