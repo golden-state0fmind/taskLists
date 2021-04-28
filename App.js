c89a11205d47973f772d10c8252582ada9d9b8e1
golden-state0fmind <areyes2009@yahoo.com>
2021-04-28 15:30:20 -0700

    adds navigation and screen stack

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import ColorBox from './components/ColorBox';
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './screens/Home'
+import ColorPallete from './screens/ColorPallete'
+import {createStackNavigator} from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex:1}} >
-      <View style={styles.container}>
-        <Text style={styles.text}>Color Legend</Text>
-        <StatusBar style="auto" />
-        <ColorBox colorName="Cyan" hexCode="#2aa198" />
-        <ColorBox colorName="Magenta" hexCode="#d33682" />
-        <ColorBox colorName="Blue" hexCode="#268bd2" />
-        <ColorBox colorName="Orange" hexCode="#cb4b16" />
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <StatusBar />
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="ColorPallete" component={ColorPallete} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    paddingTop:40,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom:10,
-  },
-});
